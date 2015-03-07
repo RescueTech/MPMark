73e952921d562d717eb1d823a38f6d8ef3cca1c0
Ahmed Saad <ahmeds.elghareeb@gmail.com>
2015-03-07 15:35:42 +0200

    refactored main FT

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,25 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class ItemCRUDTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(5)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_add_an_item_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        # user opens the site to find their supermarket's
+        # name in the page title.
+        self.assertIn('MPMark V01', self.browser.title)
+        # he also sees a link to add new products
+        # and another link to see all existing products.
+        # he addes a new product
+        # he goes back to see the products he added
+        # and sees a list with one item in it
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
